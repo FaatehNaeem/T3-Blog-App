76926a04727a0805c19ae9652df316ba139ed0da
ProgrammerFaateh90 <faatehnaeem53@gmail.com>
2024-09-29 15:20:52 +0500

    drizzle changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig} from "drizzle-kit";
-
 import { env } from "~/env";
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   schema: "./src/server/db/schema.ts",
   dialect: "postgresql",
-  out:"./drizzle",
+  out: "./drizzle",
   dbCredentials: {
     url: env.DATABASE_URL,
   },
-  tablesFilter: ["my-blog-app_*"],
-})
+  verbose: true,
+  strict: true,
+});
