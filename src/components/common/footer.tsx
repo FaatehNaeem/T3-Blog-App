8a02367b0a6e6d337029069410b7c94d788ad657
ProgrammerFaateh90 <faatehnaeem53@gmail.com>
2024-10-03 15:27:08 +0500

    added footer component

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,11 +1,82 @@
+import { Package2 } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+const techStack = ["T", "E", "C", "H", "S", "T", "A", "C", "K"];
 export default function Footer() {
   return (
-    <div className="h-2/6 w-screen bg-zinc-700 p-24 text-white">
-      <div>
-        <h1>Logo</h1>
-        <h3>Footer</h3>
+    <>
+      <div className="flex h-2/4 w-screen flex-row bg-zinc-700 text-white">
+        {/* tech stack 1 */}
+        <div className="flex-col">
+          <div className="flex h-2/4 w-[300px] flex-col items-center justify-center bg-black">
+            <Image src="/favicon.ico" alt="Image" width="20" height="20" />
+            <h3>MY Blog App</h3>
+          </div>
+
+          <div className="flex h-2/4 w-[300px] flex-col items-center justify-center bg-black">
+            <Image src="/favicon.ico" alt="Image" width="20" height="20" />
+            <h3>MY Blog App</h3>
+          </div>
+        </div>
+
+        {/* Tech stack container  */}
+        <div className="flex h-full w-11 flex-col items-center justify-center bg-red-600">
+          {techStack.map((stack, index) => (
+            <h1 key={index} className="mt-[6px] font-extrabold">
+              {stack}
+            </h1>
+          ))}
+        </div>
+
+        {/* tech stack 2 */}
+        <div className="flex-col">
+          <div className="flex h-2/4 w-[300px] flex-col items-center justify-center bg-black">
+            <Image src="/favicon.ico" alt="Image" width="20" height="20" />
+            <h3>MY Blog App</h3>
+          </div>
+
+          <div className="flex h-2/4 w-[300px] flex-col items-center justify-center bg-black">
+            <Image src="/favicon.ico" alt="Image" width="20" height="20" />
+            <h3>MY Blog App</h3>
+          </div>
+        </div>
+
+        {/* border between  */}
+        <div className="border-2 border-red-600"></div>
+
+        <div className="h-full w-[48%] flex-col">
+          <div className="flex h-1/3 w-full flex-row items-center justify-center gap-4 bg-black">
+            <Image src="/favicon.ico" alt="Image" width="20" height="20" />
+            <h3>MY Blog App</h3>
+          </div>
+
+          <div className="flex h-1/3 w-full flex-row items-center justify-center gap-4 bg-black">
+            <Link
+              href="#"
+              className="text-white transition-colors hover:text-zinc-300"
+            >
+              Home
+            </Link>
+            <Link
+              href="#"
+              className="text-white transition-colors hover:text-zinc-300"
+            >
+              About
+            </Link>
+            <Link
+              href="#"
+              className="text-white transition-colors hover:text-zinc-300"
+            >
+              Categories
+            </Link>
+          </div>
+
+          <div className="flex h-1/3 w-full flex-row items-center justify-center bg-black">
+            <h3>&copy;Copyright. All Rights Reserved</h3>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
-
