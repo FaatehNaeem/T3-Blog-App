fd57e7c8f867a2b4bb2375695c404b47da07ceb3
ProgrammerFaateh90 <faatehnaeem53@gmail.com>
2024-10-30 17:09:06 +0500

    added routes to dashboard and create blog

---
+++ src/app/Dashboard/create-blog/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div className='bg-background w-screen h-screen flex items-center'>
+    <p>Create a post in here</p>
+</div>  )
+}
+
