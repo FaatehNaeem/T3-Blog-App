e3d5598aa034bea4685f7b8db783f69a5128c431
ProgrammerFaateh90 <faatehnaeem53@gmail.com>
2024-11-17 18:29:00 +0500

    added select instead of dropdown in blog fomr

--- src/components/forms/blog-post-form.tsx
+++ src/components/forms/blog-post-form.tsx
@@ -5,12 +5,15 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { type z } from "zod";
 import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-} from "~/components/ui/dropdown-menu";
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectLabel,
+} from "~/components/ui/select";
+
 import { Textarea } from "~/components/ui/textarea";
 import { Button } from "~/components/ui/button";
 import {
@@ -22,22 +25,20 @@ import {
   FormMessage,
 } from "~/components/ui/form";
 import { Input } from "~/components/ui/input";
-import {ChevronDown} from "lucide-react"
+import { ChevronDown } from "lucide-react";
 import { api } from "~/trpc/react";
-import { useSession } from "next-auth/react";
 
 export default function BlogPostForm() {
-  const [submitting,isSubmitted] = useState(false)
-  const utils = api.useUtils()
+  const [submitting, isSubmitted] = useState(false);
+  const utils = api.useUtils();
   // const {data:session} = useSession()
   // const userId = session?.user.id;
 
-
-  const {mutate} = api.blog.createBlog.useMutation({
-    onSuccess:async()=>{
+  const { mutate } = api.blog.createBlog.useMutation({
+    onSuccess: async () => {
       await utils.blog.invalidate();
-    }
-  })
+    },
+  });
 
   const form = useForm<z.infer<typeof BlogPostSchema>>({
     resolver: zodResolver(BlogPostSchema),
@@ -45,30 +46,29 @@ export default function BlogPostForm() {
       title: "",
       description: "",
       category: "",
-      blogImage: ""
+      blogImage: "",
     },
   });
 
-   const onSubmit=async(values: z.infer<typeof BlogPostSchema>) =>{
+  const onSubmit = async (values: z.infer<typeof BlogPostSchema>) => {
     // if (!session?.user?.id) {
     //   alert("User not logged in");
     //   return;
     // }
     // console.log(session.user.id)
     await mutate({
-          title:values.title,
-          category:values.category,
-          description:values.description,
-          blogImage:values.blogImage,
-        });
-
-  }
+      title: values.title,
+      category: values.category,
+      description: values.description,
+      blogImage: values.blogImage,
+    });
+  };
 
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="h-screen w-2/4 rounded-2xl border border-background bg-foreground text-background p-6"
+        className="h-screen w-2/4 rounded-2xl border border-background bg-foreground p-6 text-background"
       >
         {/* <h1 className="text-background mb-4">Create your blog post</h1> */}
         <FormField
@@ -80,7 +80,7 @@ export default function BlogPostForm() {
               <FormControl>
                 <Input placeholder="Enter a title" {...field} />
               </FormControl>
-              <FormMessage className="h-6"/>
+              <FormMessage className="h-6" />
             </FormItem>
           )}
         />
@@ -97,7 +97,7 @@ export default function BlogPostForm() {
                   {...field}
                 />
               </FormControl>
-              <FormMessage className="h-6"/>
+              <FormMessage className="h-6" />
             </FormItem>
           )}
         />
@@ -107,31 +107,25 @@ export default function BlogPostForm() {
           render={({ field }) => (
             <FormItem className="mt-4 h-28">
               <FormLabel>Catgory</FormLabel>
-              <FormControl>
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <div className="flex flex-row items-start w-full">
-                      <Input
-                        type="text"
-                        placeholder="Select a category"
-                        className="w-screen"
-                        {...field}
-                      />
-                      <ChevronDown className="relative right-6 top-1"/>
-                    </div>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent className="w-56">
-                    <DropdownMenuGroup>
-                      <DropdownMenuItem>Fashion</DropdownMenuItem>
-                      <DropdownMenuItem>Technology</DropdownMenuItem>
-                      <DropdownMenuItem>Health</DropdownMenuItem>
-                      <DropdownMenuItem>Lifestyle</DropdownMenuItem>
-                      <DropdownMenuItem>Coding</DropdownMenuItem>
-                    </DropdownMenuGroup>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </FormControl>
-              <FormMessage className="h-6"/>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select a category" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Categories</SelectLabel>
+                    <SelectItem value="Fashion">Fashion</SelectItem>
+                    <SelectItem value="Technology">Technology</SelectItem>
+                    <SelectItem value="Health">Health</SelectItem>
+                    <SelectItem value="Lifestyle">Lifestyle</SelectItem>
+                    <SelectItem value="Coding">Coding</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+
+              <FormMessage className="h-6" />
             </FormItem>
           )}
         />
@@ -150,13 +144,13 @@ export default function BlogPostForm() {
                   className="text-center"
                 />
               </FormControl>
-              <FormMessage className="h-6"/>
+              <FormMessage className="h-6" />
             </FormItem>
           )}
         />
         <Button
           type="submit"
-          className="bg-background text-foreground hover:bg-background hover:opacity-90 mt-2 w-full"
+          className="mt-2 w-full bg-background text-foreground hover:bg-background hover:opacity-90"
         >
           Submit
         </Button>
