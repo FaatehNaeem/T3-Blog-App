8342877f1a773de5877fa8da48376fce29e5254b
FaatehNaeem <faatehnaeem53@gmail.com>
2025-07-05 12:40:19 +0500

    changed cross icon in input search bar

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -4,12 +4,12 @@ import { Search } from "lucide-react";
 export default function SearchBar() {
   return (
     <form className="flex h-12 items-center justify-center mt-2">
-      <div className="relative rounded-lg bg-background">
-        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-primary" />
+      <div className="relative rounded-lg">
+        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-foreground" />
         <Input
           type="search"
           placeholder="Search Blogs..."
-          className="border-foreground pl-8 text-background shadow-foreground shadow-sm placeholder:text-background focus-visible:ring-0 sm:w-[300px] md:w-[200px] lg:w-[300px] bg-foreground"
+          className="border-foreground pl-8 text-foreground shadow-sm placeholder:text-foreground focus-visible:ring-0 sm:w-[300px] md:w-[200px] lg:w-[300px] bg-transparent"
         />
       </div>
     </form>
