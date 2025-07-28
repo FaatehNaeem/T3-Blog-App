f5570f790793c52f2470592ff450617a4843862a
FaatehNaeem <faatehnaeem53@gmail.com>
2025-07-28 23:26:44 +0500

    added NODE_ENV to resolve build error

--- src/env.js
+++ src/env.js
@@ -5,11 +5,13 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
     NEXTAUTH_SECRET: z.string().min(1),
+    NODE_ENV:z.string().min(1),
   },
   client: {
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NODE_ENV:process.env.NODE_ENV
   },
 });
